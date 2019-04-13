c35e5f90584fd64b2fd4b77d5f4a097072b56424
CYBERXYZ <49577970+CYBERXYZ@users.noreply.github.com>
2019-04-13 10:37:27 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "97d7785e-3acf-417f-89ea-e91a0197c9dd";
+//user_token wajib di isi //
+$user_token="AbyIuFzW1DQuxmfz";
+
+//user_id wajib di isi //
+$user_id="Facebook_121966402312219";
+
+// invite_code wajib di isi //
+$invite_code="FYdHmsVZr";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xMjE5NjY0MDIzMTIyMTkiLCJleHAiOiIxNTU3Njg4NDAwNjAzIn0.qOR-E7OnN4Rj0JyDQ601bpzrnOx5eacewiYQOITROGU";
+
+
+
+?>
